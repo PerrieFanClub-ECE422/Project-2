fd0ed72374d85ec96da29cb03484121ef667c15c
Ubuntu <sepniojr@ualberta.ca>
2024-03-20 23:14:18 +0000

    ...

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import os
 import shutil
-
+import pycrypto
 
 class User:
     # user is saved to database
@@ -13,14 +13,16 @@ class User:
 
 class File:
     # user is saved to database
-    def __init__(self, name):
+    def __init__(self, name, owner):
         self.name = name
+        self.owner = owner
 
 
 class Directory:
     # user is saved to database
-    def __init__(self, name):
+    def __init__(self, name, owner):
         self.name = name
+        self.owner = owner
 
 
 def create_user(name):
@@ -28,8 +30,8 @@ def create_user(name):
     # do some database stuff
     return user
 
-def create_dir(name):
-    dir = Directory(name)
+def create_dir(name, owner):
+    dir = Directory(name, owner)
     # do some database stuff
     return dir
 
@@ -47,13 +49,43 @@ def main():
         cmd = input("\nSFS$ : ")
 
 
+    """
+
+    Your SFS should support common commands in the Linux file system (while you
+        can name your own commands). At least the following commands should be
+        supported:
+        ○ pwd: see what directory you are currently in,
+        ○ ls: list the files in current directory,
+        ○ cd: change directory,
+        ○ mkdir: make a new subdirectory,
+        ○ touch: create a new file,
+        ○ cat: read a file,
+        ○ echo: write to a file,
+        ○ mv: rename a file (however you are not required to support moving files
+        between different directories)
+        Keep in mind that you need to check permissions whenever a user executes
+        these commands.
+
+    """
+
         # switch statements using input cmd
+        # check permissions whenever a user executes these commands
         if cmd == "ls":
             command_ls()
+
+def login():
+    currentuser_name = input("Username: ")
+
+    # Check if user exists in db
+
+    currenuser_pass = input("Pass: ")
+
+    # Check if password is correct -> do some encryption/decryption on db side
 
 
 if __name__ == '__main__':
     print("Welcome to the SFS")
     print("<Authentication stuff>")
+
     # we do some authentication before we go to main
     main()
