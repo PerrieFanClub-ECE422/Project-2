2f5a5df9d64601169ea64f73432b8a0939985b3a
Ubuntu <sepniojr@ualberta.ca>
2024-03-20 22:48:21 +0000

    fsdfdsfds

---
+++ main.py
@@ -0,0 +1,59 @@
+
+import os
+import shutil
+
+
+class User:
+    # user is saved to database
+    def __init__(self, name):
+        self.name = name
+        self.home_dir = create_dir(name)         # user should also have their own directory created
+
+
+
+class File:
+    # user is saved to database
+    def __init__(self, name):
+        self.name = name
+
+
+class Directory:
+    # user is saved to database
+    def __init__(self, name):
+        self.name = name
+
+
+def create_user(name):
+    user = User(name)
+    # do some database stuff
+    return user
+
+def create_dir(name):
+    dir = Directory(name)
+    # do some database stuff
+    return dir
+
+
+
+def command_ls():
+    files = os.listdir('.')
+    for file in files: 
+        print(file, end="  ")
+
+
+
+def main():
+    while True:
+        cmd = input("\nSFS$ : ")
+
+
+        # switch statements using input cmd
+        if cmd == "ls":
+            command_ls()
+
+
+if __name__ == '__main__':
+    print("Welcome to the SFS")
+    print("<Authentication stuff>")
+    # we do some authentication before we go to main
+    main()
