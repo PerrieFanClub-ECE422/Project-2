5fb6518bce4baf4adc5d733ec01d330d4812bd40
Ubuntu <sepniojr@ualberta.ca>
2024-03-27 19:22:32 +0000

    added empty cmd functions

--- commands.py
+++ commands.py
@@ -1,8 +1,31 @@
 import os
 
+
+def pwd(): # see current directory
+    return 
+
 def ls():
     files = os.listdir('.')
     for file in files: 
         print(file, end="  ")
 
+
+
+def cd(): # change dir
+    return 
+
+def mkdir(): # make new subdir in current directory
+    return 
+
+def touch(): # create a new file (txt)
+    return 
+
+def cat(): #read a file
+    return
+
+def echo(): #write to a file
+    return
+
+def mv(): #rename a file
+    return 
 
